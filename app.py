0d4d70863bd2507d4c12ddeb6aa57bda650ab0e2
James Nam <jnam@jnam-mbp.local>
2013-07-21 17:53:05 -0700

    Improved the file tree.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os, sys
 app = Flask(__name__)
 
 @app.route('/')
--- compiler.py
+++ compiler.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python
 import os, sys, json, webbrowser
 
-# 1. compile .coffee files
-command = "coffee --compile --output static/_coffee/ scripts/"
-os.system(command)
-
-# 2. pre-compile .handlebars files
-templatesListFilename = "scripts/templatesList.json"
-jsonData = open(templatesListFilename).read()
-data = json.loads(jsonData)
-
-origPathPrefix = "scripts/templates/"
-newPathPrefix = "static/_handlebars/"
-
-for origName in data:
-  newName = ".".join(origName.split(".")[:-1]) + ".js"
-  origPath = origPathPrefix + origName
-  newPath = newPathPrefix + newName
-  command = "handlebars %s -f %s" % (origPath, newPath)
+def compileCoffee():
+  command = "coffee --compile --output static/js/ scripts/"
   os.system(command)
 
-# 3. launch website
-url = "http://localhost:5000"
-webbrowser.open(url, new = 0)
+def compileHandlebars():
+  templatesListFilename = "scripts/templatesList.json"
+  jsonData = open(templatesListFilename).read()
+  data = json.loads(jsonData)
+
+  origPathPrefix = "scripts/templates/"
+  newPathPrefix = "static/templates/"
+
+  for origName in data:
+    newName = ".".join(origName.split(".")[:-1]) + ".js"
+    origPath = origPathPrefix + origName
+    newPath = newPathPrefix + newName
+    command = "handlebars %s -f %s" % (origPath, newPath)
+    os.system(command)
+
+def compileLess():
+  stylesListFilename = "scripts/stylesList.json"
+  jsonData = open(stylesListFilename).read()
+  data = json.loads(jsonData)
+
+  origPathPrefix = "scripts/styles/"
+  newPathPrefix = "static/styles/"
+
+  for origName in data:
+    newName = ".".join(origName.split(".")[:-1]) + ".css"
+    origPath = origPathPrefix + origName
+    newPath = newPathPrefix + newName
+    command = "lessc %s %s" % (origPath, newPath)
+    os.system(command)
+
+def launch():
+  url = "http://localhost:5000"
+  webbrowser.open(url, new = 0)  
+
+compileCoffee()
+compileHandlebars()
+compileLess()
+launch()
